1182c81b09e544869d62fd55c695cfe18bda8d27
Daria Guy <daria.guy@redis.com>
2026-06-03 17:53:14 +0300

    Make LTO string and disable only for mac

--- Formula/r/redis.rb
+++ Formula/r/redis.rb
@@ -55,7 +55,7 @@ class Redis < Formula
     # Set build flags
     ENV["BUILD_WITH_MODULES"] = "yes"
     ENV["BUILD_TLS"] = "yes"
-    ENV["LTO"] = 0
+    ENV["LTO"] = "0" if OS.mac?
 
     system "make", "install", "PREFIX=#{prefix}", "CC=#{ENV.cc}", "IGNORE_MISSING_DEPS=1"
 
