eee0bf4f104ae9b5da8f8b5dea8c9927690ea6f3
Daria Guy <daria.guy@redis.com>
2026-06-04 11:48:01 +0300

    Use cd instead of resources

--- Formula/r/redis.rb
+++ Formula/r/redis.rb
@@ -1,10 +1,10 @@
 class Redis < Formula
   desc "Persistent key-value database, with built-in net interface"
   homepage "https://redis.io/"
-  url "https://download.redis.io/releases/redis-8.8.0.tar.gz"
-  sha256 "88422181efb0c9c0abba332e3e391d409e1e13714b838931669235e5796f704b"
+  url "https://github.com/dariaguy/redis/releases/download/8.8.0/redis-8.8.0.tar.gz"
+  sha256 "f286dc0cf5005d8503f30851a25ea657b8cca212afb4d936d3ebeae48531962e"
   license all_of: [
-    "AGPL-3.0-only",
+    "AGPL-3.0-only", # modules
     "BSD-2-Clause", # deps/jemalloc, deps/linenoise, src/lzf*
     "BSL-1.0", # deps/fpconv
     "MIT", # deps/lua
@@ -18,8 +18,6 @@ class Redis < Formula
     url "https://download.redis.io/releases/"
     regex(/href=.*?redis[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
-
-  no_autobump! because: :bumped_by_upstream
 
   bottle do
     sha256 cellar: :any,                 arm64_tahoe:   "2a6d55f7fe58eed0accecb5b313ad04e40931e4d5a9bb0970b0ddc1aee8bac7f"
@@ -46,42 +44,22 @@ class Redis < Formula
 
   conflicts_with "valkey", because: "both install `redis-*` binaries"
 
-  resource "redisjson" do
-    url "https://github.com/redisjson/redisjson.git",
-    revision: "107144fd2c0a6b325108352bf83ed6e6f731a20f"
-  end
-
-  resource "redisbloom" do
-    url "https://github.com/redisbloom/redisbloom.git",
-    revision: "60c96b3f11dcf71d4707137a3452bbb6941493dd"
-  end
-
-  resource "redistimeseries" do
-    url "https://github.com/redistimeseries/redistimeseries.git",
-    revision: "5d7c61c9f861b5cb83989463595c2c9f6b2bfe63"
-  end
-
-  resource "redisearch" do
-    url "https://github.com/redisearch/redisearch.git",
-    revision: "30204860f145da013ba042e0824df6d344aef4ce"
-  end
-
   def install
     openssl = Formula["openssl@3"]
 
     system "make", "install", "PREFIX=#{prefix}", "CC=#{ENV.cc}", "BUILD_TLS=yes"
 
-    resource("redisjson").stage do
+    cd "modules/redisjson/src" do
       system "gmake", "all"
       lib.install Dir.glob("bin/*-release/rejson.so").first
     end
 
-    resource("redisbloom").stage do
+    cd "modules/redisbloom/src" do
       system "gmake", "all"
       lib.install Dir.glob("bin/*-release/redisbloom.so").first
     end
 
-    resource("redistimeseries").stage do
+    cd "modules/redistimeseries/src" do
       # Set compiler flags for OpenSSL
       ENV.append "CFLAGS", "-I#{openssl.opt_include}"
       ENV.append "CXXFLAGS", "-I#{openssl.opt_include}"
@@ -92,7 +70,7 @@ class Redis < Formula
       lib.install Dir.glob("bin/*-release/redistimeseries.so").first
     end
 
-    resource("redisearch").stage do
+    cd "modules/redisearch/src" do
       # RediSearch has been verified to support runtime CPU detection for SIMD optimizations
       ENV.runtime_cpu_detection
       # Build the module
