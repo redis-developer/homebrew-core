3ef6ee78f1c1f21cbc04cd858722858a9be45dcc
Daria Guy <daria.guy@redis.com>
2026-07-07 12:56:23 +0300

    Replace modules files path in the conf

--- Formula/r/redis.rb
+++ Formula/r/redis.rb
@@ -58,6 +58,7 @@ class Redis < Formula
       s.gsub! "/var/run/redis_6379.pid", var/"run/redis.pid"
       s.gsub! "dir ./", "dir #{var}/db/redis/"
       s.sub!(/^bind .*$/, "bind 127.0.0.1 ::1")
+      s.gsub! "#{prefix}/lib/redis/modules", "#{opt_prefix}/lib/redis/modules"
     end
 
     etc.install "redis.conf"
